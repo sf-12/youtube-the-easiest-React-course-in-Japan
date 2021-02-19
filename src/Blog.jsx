8d9c77b4871112349b2e4b028c755e954eb00c7f
sf-12 <s.fukasawa12@gmail.com>
2021-02-19 14:14:48 +0900

    useEffectでライフサイクルを管理

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,59 +3,42 @@ import Article from './Article';
 import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge';
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+const Blog = () => {
 
-  componentDidMount() {
-    // ボタンがクリックされたらいいねをカウントアップする
-    document.getElementById('counter').addEventListener('click', () => this.countUP());
-  }
+  // componentDidMount() {
+  //   // ボタンがクリックされたらいいねをカウントアップする
+  //   document.getElementById('counter').addEventListener('click', () => this.countUP());
+  // }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10 ) {
-      this.setState ({ count: 0 })
-    }
-  }
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10 ) {
+  //     this.setState ({ count: 0 })
+  //   }
+  // }
 
-  componentWillUnmount() {
-    document.getElementById('counter').removeEventListener('click', () => this.countUP());
-  }
+  // componentWillUnmount() {
+  //   document.getElementById('counter').removeEventListener('click', () => this.countUP());
+  // }
 
-  // 公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState ( state => {
-      return { isPublished: !this.state.isPublished }
-    })
-  }
+  // countUP = () => {
+  //   this.setState(state => {
+  //     return { count: this.state.count + 1 }
+  //   })
+  // }
 
-  countUP = () => {
-    this.setState(state => {
-      return { count: this.state.count + 1 }
-    })
-  }
+  const authorName = "Torahack"
+  return (
+    <>
+      <Article
+        title={'Reactの使い方'}
+        // count={this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </>
+  )
 
-  render() {
-    const authorName = "Torahack"
-    return (
-      <>
-        <Article
-          title={'Reactの使い方'}
-          isPublished={this.state.isPublished}
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
-        />
-        <FooBar.Foo />
-        <FooBar.Bar />
-        <Hoge />
-      </>
-    )
-  }
 }
 
 export default Blog
